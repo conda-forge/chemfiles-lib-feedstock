59c9a5e014fedf9f170433a497d8aa136da2b2f0
Luthaf <luthaf@luthaf.fr>
2016-10-19 22:45:01 +0200

    Activate windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,8 @@
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCHFL_ENABLE_NETCDF=ON .
+cmake --build . --config release --target install
+
+:: Build and run the tests
+cmake -E copy %RECIPE_DIR%/27d89caa55e1f42cdbbed5a322b51c702066d871.tar.gz tests/
+cmake -DCHFL_BUILD_TESTS=ON .
+cmake --build . --config release
+ctest --output-on-failure -C release
