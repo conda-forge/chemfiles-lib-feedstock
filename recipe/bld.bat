91e825f02a73fedb3bbb86085ee3142c7af678ca
Luthaf <luthaf@luthaf.fr>
2017-12-05 22:55:27 +0100

    Update to 0.8.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ cmake -G "%CMAKE_GENERATOR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREF
 cmake --build . --config release --target install
 
 :: Build and run the tests
-cmake -E copy %RECIPE_DIR%/27d89caa55e1f42cdbbed5a322b51c702066d871.tar.gz tests/
+cmake -E copy %RECIPE_DIR%/c26f254cd7bff7e476bf065403fbd0a6a47fea38.tar.gz tests/
 cmake -DCHFL_BUILD_TESTS=ON .
 cmake --build . --config release
 ctest --output-on-failure -C release
