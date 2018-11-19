3cf049babc3002d02a807455a80ba0ffc7d13708
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-11-19 18:33:26 +0000

    Update to 0.9.0-rc1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ cmake  -G "$CMAKE_GENERATOR" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREF
 cmake --build . --config release --target install
 
 # Build and run the tests
-cmake -E touch tests/data/c26f254cd7bff7e476bf065403fbd0a6a47fea38
+cmake -E touch tests/data/dee3659fad8106611a9fd0f0a5ee4596f7fb869d
 cmake -DCHFL_BUILD_TESTS=ON .
 cmake --build . --config release
 ctest --output-on-failure -C release
