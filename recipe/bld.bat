2979285220f3bac1097140e3c74b71e8aa46d7ee
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-11-20 13:26:35 +0000

    Use Release instead of release as build configuration
    This should fix a warning with vcpkg on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
-cmake -G "%CMAKE_GENERATOR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library .
-cmake --build . --config release --target install
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library .
+cmake --build . --config Release --target install
 
 :: Build and run the tests
 cmake -E touch tests\data\dee3659fad8106611a9fd0f0a5ee4596f7fb869d
 cmake -DCHFL_BUILD_TESTS=ON .
-cmake --build . --config release
-ctest --output-on-failure -C release
+cmake --build . --config Release
+ctest --output-on-failure -C Release
