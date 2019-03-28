7efb60484abff1d5aefa99d5b599e3bf006c6191
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2019-03-28 16:19:46 +0100

    Improve build script and metadata

--- recipe/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%/Library .
-cmake --build . --config Release --target install
-
-:: Build and run the tests
-cmake -E touch tests\data\dee3659fad8106611a9fd0f0a5ee4596f7fb869d
-cmake -DCHFL_BUILD_TESTS=ON -DCHFL_BUILD_DOCTESTS=OFF .
-cmake --build . --config Release
-ctest --output-on-failure -C Release
